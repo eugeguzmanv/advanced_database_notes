b932d6bdaaa75705f378db19136b9bda0f3d2ed5
Eugenio Guzmán Vargas <eugegv03@gmail.com>
2026-04-07 09:02:01 -0600

    session 07-04-2026

--- sql_challenges/challenge-05/README.md
+++
@@ -1,1 +0,0 @@
-This is the challenge 6 readme
---
+++ sql_challenges/challenge-07/solution.sql
@@ -0,0 +1,15 @@
+1. Something in the article
+Question: Where was Cristiano Ronaldo born?  
+Answer: Cristiano Ronaldo dos Santos Aveiro was born on 5 February 1985 in the São Pedro parish of Funchal, 
+Comment: It makes sense and is correct, but because of the size of the chunks it is missing some information, like the fact he was born in portugal
+
+2. Something related but not directly quoted
+Question: Who is the top goal socrer of all time in Real Madrid? 
+Answer: 1. In the 2016–17 UEFA Champions League quarter-finals against Bayern in April, Ronaldo scored both goa
+        2. Cristiano Ronaldo became Real Madrid's all-time top scorer on 12 September 2015 against Espanyol, ne
+Comment: Interestingly, the second closest answer is the answer i was looking for. The distances were 1:0.3562, 2: 0.3622. This may be because of the way the document was chunked (this is just my guess)
+
+3. Something unrelated
+Question: What is RAG? 
+Answer: Although he requested the number 28, his number at Sporting, he received the squad number 7 shirt, w
+Comment: Completeley unrelated, the distance was 0.9928. We could set a min distance so that when something is unrelated, the query returns "not found"
